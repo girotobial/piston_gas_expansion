da062e6a5b098eead3bddb896f6c3a0c7520b889
Alex Robinson <girotobial@gmail.com>
2021-08-22 23:15:19 +0100

    💄 add selectors: - add bottle pressure - add bore profile

---
+++ src/components/Controls.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card>
+    <v-card-title> Controls </v-card-title>
+    <v-card-text>
+      <v-row class="mb-4">
+        <v-col class="text-left">
+          <v-select
+            v-model="$store.state.pistonShape"
+            label="Piston Shape"
+            :items="pistonShapeItems"
+          ></v-select>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-slider
+            v-model="$store.state.airPressure"
+            min="0"
+            max="120"
+            label="Bottle Pressure (PSI)"
+            thumb-label
+            :color="airPressureColor"
+            track-color="grey"
+          >
+            <template v-slot:append>
+              <v-text-field
+                v-model="$store.state.airPressure"
+                type="number"
+                class="mt-0 pt-0"
+                style="width: 60px"
+              ></v-text-field>
+            </template>
+          </v-slider>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Controls extends Vue {
+  pistonShapeItems = ["Circular", "Square"];
+
+  get airPressureColor(): string {
+    const airPressure = this.$store.state.airPressure;
+    if (airPressure < 61) return "green";
+    if (airPressure < 91) return "orange";
+    return "red";
+  }
+}
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,7 +4,10 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    airPressure: 60,
+    pistonShape: "Circular",
+  },
   mutations: {},
   actions: {},
   modules: {},
