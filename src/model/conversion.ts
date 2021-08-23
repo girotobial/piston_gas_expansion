f1b86c5c4d45db5d67ff88f57a7d1751329aa5cb
Alex Robinson <girotobial@gmail.com>
2021-08-23 21:50:29 +0100

    🐛 round output of pressure to 2 decimal places

--- src/model/conversion.ts
+++ src/model/conversion.ts
@@ -1,5 +1,6 @@
 export const pressurePsiToPascal = (pressure: number): number => {
-  return pressure * 6894.76;
+  const pascal = pressure * 6894.76;
+  return Math.round(pascal * 100) / 100;
 };
 export const millimetreToMetre = (millimetre: number): number => {
   return millimetre / 1000;
