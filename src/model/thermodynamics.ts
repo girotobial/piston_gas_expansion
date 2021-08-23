500b59f465dcd4db3ea8162f5b5a3b5c7b32bfd5
Alex Robinson <girotobial@gmail.com>
2021-08-23 23:26:20 +0100

    ✨ add expansion strategies

--- src/model/thermodynamics.ts
+++ src/model/thermodynamics.ts
@@ -1,4 +1,4 @@
-export interface Expansion {
+interface Expansion {
   endPressure(
     startPressure: number,
     startVolume: number,
@@ -10,3 +10,56 @@ export interface Expansion {
     endVolume: number
   ): number;
 }
+
+const sigFigs = (value: number, sigFigs: number) => {
+  return parseFloat(value.toPrecision(sigFigs));
+};
+
+export class Isobaric implements Expansion {
+  constructor() { } // eslint-disable-line
+
+  endPressure(startPressure: number, startVolume: number, endVolume: number): number { // eslint-disable-line
+    return startPressure;
+  }
+
+  workDone(
+    startPressure: number,
+    startVolume: number,
+    endVolume: number
+  ): number {
+    const deltaV = endVolume - startVolume;
+    return startPressure * deltaV;
+  }
+}
+
+class Adiabatic implements Expansion {
+  gamma: number;
+
+  constructor(gamma = 1.4) {
+    this.gamma = gamma;
+  }
+
+  endPressure(
+    startPressure: number,
+    startVolume: number,
+    endVolume: number
+  ): number {
+    const pressureRatio = startVolume / endVolume;
+    return sigFigs(startPressure * pressureRatio ** this.gamma, 3);
+  }
+
+  workDone(
+    startPressure: number,
+    startVolume: number,
+    endVolume: number
+  ): number {
+    const endPressure = this.endPressure(startPressure, startVolume, endVolume);
+    const gammaRatio = 1 / (1 - this.gamma);
+    const energyDifference =
+      endVolume * endPressure - startVolume * startPressure;
+
+    return sigFigs(gammaRatio * energyDifference, 3);
+  }
+}
+
+export { Adiabatic };
