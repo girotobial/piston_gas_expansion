5d1b5d6fe21d7eba98ab9dc2de9e691378018184
Alex Robinson <girotobial@gmail.com>
2021-08-26 16:53:10 +0100

    ✨ add piston bore

--- src/model/mechanics.ts
+++ src/model/mechanics.ts
@@ -1,3 +1,37 @@
+import { CrossSection } from "./geometry";
+
 export const pressureForce = (pressure: number, area: number): number => {
   return pressure * area;
 };
+
+export class PistonBore {
+  crossSection: CrossSection;
+  tdcLength: number;
+  strokeLength: number;
+
+  constructor(
+    crossSection: CrossSection,
+    tdcLength: number,
+    strokeLength: number
+  ) {
+    this.crossSection = crossSection;
+    this.tdcLength = tdcLength;
+    this.strokeLength = strokeLength;
+  }
+
+  strokeVolume(): number {
+    return this.crossSection.area() * this.strokeLength;
+  }
+
+  tdcVolume(): number {
+    return this.crossSection.area() * this.tdcLength;
+  }
+
+  bdcVolume(): number {
+    return this.crossSection.area() * (this.tdcLength + this.strokeLength);
+  }
+
+  compressionRatio(): number {
+    return this.bdcVolume() / this.tdcVolume();
+  }
+}
