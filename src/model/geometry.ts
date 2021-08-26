297eb25fb7b35d6c759114f33746e501a40b3057
Alex Robinson <girotobial@gmail.com>
2021-08-26 17:04:31 +0100

    🔥 remove ThreeDimensionalShapes

--- src/model/geometry.ts
+++ src/model/geometry.ts
@@ -1,10 +1,3 @@
-export interface ThreeDimensionalShape {
-  width: number;
-  height: number;
-  crossSectionArea(): number;
-  volume(): number;
-}
-
 export interface CrossSection {
   width: number;
   area(): number;
@@ -36,62 +29,10 @@ export class SquareCrossSection implements CrossSection {
   constructor(width: number) {
     this.width = width;
   }
-
   area() {
     return this.width ** 2;
   }
-
   perimeter() {
     return this.width * 4;
   }
 }
-
-class Cylinder implements ThreeDimensionalShape {
-  diameter: number;
-  height: number;
-
-  get width() {
-    return this.diameter;
-  }
-
-  constructor(diameter: number, height: number) {
-    this.diameter = diameter;
-    this.height = height;
-  }
-  crossSectionArea(): number {
-    return (0.5 * this.diameter) ** 2 * Math.PI;
-  }
-  volume(): number {
-    return this.crossSectionArea() * this.height;
-  }
-}
-
-class Cuboid implements ThreeDimensionalShape {
-  width: number;
-  height: number;
-
-  constructor(width: number, height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  crossSectionArea(): number {
-    return this.width ** 2;
-  }
-  volume(): number {
-    return this.height * this.crossSectionArea();
-  }
-}
-
-export const create3dShape = (
-  crossSection = "circular",
-  width: number,
-  height: number
-): ThreeDimensionalShape => {
-  crossSection = crossSection.toLowerCase();
-  switch (crossSection) {
-    case "square":
-      return new Cuboid(width, height);
-    default:
-      return new Cylinder(width, height);
-  }
-};
