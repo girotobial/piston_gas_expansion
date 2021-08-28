85dc89f4b8b3408a500b326faaf7ff8a264e6cf0
Alex Robinson <girotobial@gmail.com>
2021-08-28 19:17:21 +0100

    add Piston interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -2,3 +2,10 @@ export interface Bottle {
   pressure: number;
   capacity: number;
 }
+
+export interface Piston {
+  tdcLength: number;
+  strokeLength: number;
+  boreWidth: number;
+  shape: string;
+}
