844f907364981288b5455a4e9bcf75ab66eebd64
girotobial <girotobial@gmail.com>
2021-09-02 23:24:40 +0100

    ✨ add physics model

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,3 +2,50 @@ export * as conversion from "./conversion";
 export * as geometry from "./geometry";
 export * as mechanics from "./mechanics";
 export * as thermodynamics from "./thermodynamics";
+
+import { PistonBore } from "./mechanics";
+import { Expansion } from "./thermodynamics";
+import { Bottle } from "@/interfaces";
+import { atmosphericPressurePa } from "./conversion";
+
+export class Model {
+  protected _piston: PistonBore;
+  protected _expansion: Expansion;
+  protected _bottle: Bottle;
+
+  constructor(piston: PistonBore, expansion: Expansion, bottle: Bottle) {
+    this._piston = piston;
+    this._expansion = expansion;
+    this._bottle = bottle;
+  }
+
+  get piston(): PistonBore {
+    return this._piston;
+  }
+
+  get expansion(): Expansion {
+    return this._expansion;
+  }
+
+  get bottle(): Bottle {
+    return this._bottle;
+  }
+
+  pressure(proportionOfTravel: number): number {
+    const startPressure = this.bottle.pressure;
+    const startVolume = this.piston.tdcVolume();
+    const endVolume = this.piston.volume(proportionOfTravel);
+    return this.expansion.endPressure(startPressure, startVolume, endVolume);
+  }
+
+  volume(proportionOfTravel: number): number {
+    return this.piston.volume(proportionOfTravel);
+  }
+
+  torque(crankAngle: number): number {
+    const proportionOfTravel =
+      this.piston.pistonTravel(crankAngle) / this.piston.strokeLength;
+    const pressure = this.pressure(proportionOfTravel) - atmosphericPressurePa;
+    return this.piston.torque(pressure, crankAngle);
+  }
+}
